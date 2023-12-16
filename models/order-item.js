aa4ec9fb96b4c4848386e2dcb7e73e73eb0730e2
Nguyen-huan <huan06949@gmail.com>
2023-12-16 23:17:46 +0700

    fix bug order function

--- models/order-item.js
+++ models/order-item.js
@@ -1,1 +1,15 @@
-const mongoose = require('mongoose');const orderItemSchema = mongoose.Schema({    quantity: {        type: Number,        required: true    },    dish: {        type: mongoose.Schema.Types.ObjectId,        ref: 'Dish',        // required: true    }})exports.OrderItem = mongoose.model('OrderItem', orderItemSchema);
+const mongoose = require('mongoose');
+
+const orderItemSchema = mongoose.Schema({
+    quantity: {
+        type: Number,
+        required: true
+    },
+    dish: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Dish',
+        // required: true
+    }
+})
+
+exports.OrderItem = mongoose.model('OrderItem', orderItemSchema);
